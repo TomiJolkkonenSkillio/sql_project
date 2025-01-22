c0c94c5b919e84f66450867688d0d91ad677a802
TomiJolkkonen <tomi.jolkkonen@skillio.ai>
2025-01-22 11:04:12 +0200

    kaikkien tehtävien tiedostopohjat

---
+++ src/data/analytical_queries.py
@@ -0,0 +1,25 @@
+import psycopg2
+import numpy as np
+import pandas as pd
+from config import config
+
+def basic_counts_sums():
+    pass
+
+
+def grouping_aggregations():
+    pass
+
+
+def joins_multitablequeries():
+    pass
+
+
+def nextedqueries_subqueries():
+    pass
+
+
+def advanced_analyticalqueries():
+    pass
+
+
